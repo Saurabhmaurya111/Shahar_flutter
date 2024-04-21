0428580e77eadc311a1160f70ec67de9ecb77112
Saurabh Maurya <saurabhmaurya2050@gmail.com>
2024-04-21 08:41:51 +0530

    final commit is two step is away

---
+++ lib/screens/post_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shahar/widgets/post_app_bar.dart';
+import 'package:shahar/widgets/post_bottom_bar.dart';
+
+class PostScreen extends StatelessWidget {
+  const PostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/city6.jpg'),
+          fit: BoxFit.cover,
+          opacity: 0.7,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(90), 
+            child: PostAppBar(),
+            ),
+            bottomNavigationBar: PostBottomBar(),
+      ),
+    );
+  }
+}
